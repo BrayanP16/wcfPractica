e8e46b5341eed03fb6fe44ecdb5822b28db6601c
Brayan <Brayan@BRAYAN-PC>
2021-09-28 17:56:01 -0500

    Agregar archivos de proyecto.

---
+++ wcfPractica/App_Code/IService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+// NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IService1" en el código y en el archivo de configuración a la vez.
+[ServiceContract]
+public interface IService
+{
+	[OperationContract]
+	int AddAlumno(int AlumnoId, string Alumno, int Matricula);
+	[OperationContract]
+	string GetAlumnos();
+
+}
+
+[DataContract]
+public class AlumnosDetalles
+{
+	public int AlumnoId { get; set; }
+	[DataMember]
+	public string Alumno { get; set; }
+	[DataMember]
+	public Nullable<int> Matricula { get; set; }
+}
---
+++ wcfPractica/App_Code/Service.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+// NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "Service1" en el código, en svc y en el archivo de configuración.
+public class Service : IService
+{
+    public int AddAlumno(int AlumnoId, string Alumno, int Matricula)
+    {
+        UTNEntities utn = new UTNEntities();
+        Alumnos alumdtl = new Alumnos();
+        alumdtl.AlumnoId = AlumnoId;
+        alumdtl.Alumno = Alumno;
+        alumdtl.Matricula = Matricula;
+        utn.Alumnos.Add(alumdtl);
+        int Retval = utn.SaveChanges();
+        return Retval;
+    }
+
+    public string GetAlumnos()
+    {
+        List<AlumnosDetalles> alumlst = new List<AlumnosDetalles>();
+        UTNEntities utn = new UTNEntities();
+        //liq
+        var lstAlum = from k in utn.Alumnos select k;
+
+        foreach (var item in lstAlum)
+        {
+            AlumnosDetalles alumdt = new AlumnosDetalles();
+            alumdt.AlumnoId = item.AlumnoId;
+            alumdt.Alumno = item.Alumno;
+            alumdt.Matricula = item.Matricula;
+            alumlst.Add(alumdt);
+        }
+        string json = JsonConvert.SerializeObject(alumlst);
+        return json;
+    }
+   
+
+}
